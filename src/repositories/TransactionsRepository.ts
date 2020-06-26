8117f29f9021f2b61a066836f79b22fe41c5094e
Celso Brito <celsofilho_ap@hotmail.com>
2020-06-26 16:37:55 -0300

    create, list and validate transitions

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,23 +6,58 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
+  private balance: Balance;
+
   constructor() {
     this.transactions = [];
+    this.balance = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    return this.balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    if (type === 'outcome' && value > this.balance.total) {
+      throw Error(`Actually, your cashier isn't enought.`);
+    }
+
+    const incomeSum =
+      type === 'income' ? this.balance.income + value : this.balance.income;
+
+    const outcomeSum =
+      type === 'outcome' ? this.balance.outcome + value : this.balance.outcome;
+
+    const totalSum = incomeSum - outcomeSum;
+
+    this.balance = {
+      income: incomeSum,
+      outcome: outcomeSum,
+      total: totalSum,
+    };
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
